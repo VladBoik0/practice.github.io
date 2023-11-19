ec1b1101de955e056c521ef3c85a0626647f2b68
VladBoik0 <126570729+VladBoik0@users.noreply.github.com>
2023-11-19 23:43:19 +0200

    Add files via upload

--- script/code.js
+++ script/code.js
@@ -6,3 +6,28 @@ menuIcons.addEventListener('click', ()=> {
     iconOpen.classList.toggle('hide')
     maineMenu.classList.toggle('hide')
 })
+
+// Modal window
+
+let modal = document.getElementById('contact-us')
+document.querySelectorAll('.openModal').forEach(element => {
+    element.addEventListener('click', () => {
+        if(modal.classList.contains('hide')) {
+            modal.classList.remove('hide')
+        }
+    })
+});
+
+modal.addEventListener('click', () => {
+    if(modal === event.target) {
+        modal.classList.add('hide')
+    }
+})
+
+document.querySelectorAll('.closeModal').forEach(element => {
+    element.addEventListener('click', () => {
+        if(!modal.classList.contains('hide')) {
+            modal.classList.add('hide')
+        }
+    })
+});
